4ca76bbd5d7ac79d6eed25bfb3e80b97eac6c3c8
kdlSea <tempkudalaut1@gmail.com>
2025-01-11 20:14:09 +0700

    7th redirect error commit

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -2,25 +2,32 @@
 import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
+import { handleError } from "@/lib/actions/user.action";
 
 export const createSessionClient = async () => {
-  const client = new Client()
-    .setEndpoint(appwriteConfig.endpointUrl)
-    .setProject(appwriteConfig.projectId);
+  try {
+    const session = (await cookies()).get("appwrite-sessions");
+    if (session) {
+      const client = new Client()
+        .setEndpoint(appwriteConfig.endpointUrl)
+        .setProject(appwriteConfig.projectId);
 
-  const session = (await cookies()).get("appwrite-sessions");
-  if (!session || !session.value) throw new Error("Could not find session");
+      // if (!session || !session.value) handleError(error, "Could not find session");
 
-  client.setSession(session.value);
+      client.setSession(session.value);
 
-  return {
-    get account() {
-      return new Account(client);
-    },
-    get database() {
-      return new Databases(client);
-    },
-  };
+      return {
+        get account() {
+          return new Account(client);
+        },
+        get database() {
+          return new Databases(client);
+        },
+      };
+    }
+  } catch (error) {
+    handleError(error, "No session");
+  }
 };
 
 export const createAdminClient = async () => {
